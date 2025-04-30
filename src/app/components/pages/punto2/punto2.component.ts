9f479cfd74c83e5795d61eee7a80db90c1b71b04
nicoburgos <isma.nico7842018@gmail.com>
2025-04-30 16:58:52 -0300

    Punto 2

--- src/app/components/pages/punto2/punto2.component.ts
+++ src/app/components/pages/punto2/punto2.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 interface Producto {
+  id: number;
   nombre: string;
   descripcion: string;
   img: string;
@@ -18,36 +19,70 @@ interface Producto {
 export class Punto2Component {
   productos: Producto[] = [
     {
+      id: 1,
       nombre: 'Mouse Logitech G203',
       descripcion: 'Mouse de 7000 DPI',
       img: 'assets/img/products/mouse-g203.jpg',
       precio: 40
     },
     {
+      id: 2,
       nombre: 'Auriculares Red Dragon Pandora',
       descripcion: 'Auriculares con micrófono',
       img: 'assets/img/products/auriculares-red-dragon.jpg',
       precio: 60
+    },
+    {
+      id: 3,
+      nombre: "RTX 3060",
+      descripcion: "Placa de Video 12GB Msi Ventus",
+      img: "assets/img/products/rtx-3060.jpeg",
+      precio: 150
+    },
+    {
+      id: 4,
+      nombre: "Teclado Onikuma G38",
+      descripcion: "Teclado mecánico Onikuma G38",
+      img: "assets/img/products/teclado-onikuma.jpg",
+      precio: 100
+    },
+    {
+      id: 5,
+      nombre: "Mouse Onikuma CW905",
+      descripcion: "Mouse inalámbrico profesional",
+      img: "assets/img/products/mouse-onikuma.jpg",
+      precio: 50
+    },
+    {
+      id:6,
+      nombre:"Auriculares KZ EDX PRO",
+      descripcion: "Auriculares con micrófono",
+      img: "assets/img/products/kz-edx-pro.jpg",
+      precio: 25
     }
+
   ];
 
-  carrito: Producto[] = [];
+  cart: Producto[] = [];
+  animatingCart = false;
 
-  animandoCarrito = false;
-
-  agregarAlCarrito(producto: Producto) {
-    if (!this.carrito.includes(producto)) {
-      this.carrito.push(producto);
-      this.animarCarrito();
+  addProduct(producto: Producto) {
+    if (!this.cart.includes(producto)) {
+      this.cart.push(producto);
+      this.animateCart();
     }
   }
 
-  animarCarrito() {
-    this.animandoCarrito = true;
-    setTimeout(() => this.animandoCarrito = false, 500); // Duración de la animación
+  deleteProduct(id: number) {
+    this.cart = this.cart.filter( p => p.id !== id);
   }
-  
+
+  animateCart() {
+    this.animatingCart = true;
+    setTimeout(() => this.animatingCart = false, 1000); // Duración de la animación
+  }
+
   get total(): number {
-    return this.carrito.reduce((acc, prod) => acc + prod.precio, 0);
+    return this.cart.reduce((acc, prod) => acc + prod.precio, 0);
   }
 }
